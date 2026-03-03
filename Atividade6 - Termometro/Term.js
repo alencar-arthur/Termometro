8e8a210578cd36698f94a8bc2cfb9831eda05f83
Arthur Alencar <arthuralencarpinheiro844@gmail.com>
2026-03-02 21:39:36 -0300

    .

--- Atividade6 - Termometro/Term.js
+++ Atividade6 - Termometro/Term.js
@@ -2,14 +2,26 @@ document.getElementById("verificarBtn").addEventListener("click", function() {
     let numero = parseInt(document.getElementById("numero").value);
     let situacao = parseInt(document.getElementById("situacao").value);
 
-    if(isNaN(numero)) {
-        alert("Por Favor, insira números válidos.");
-        return;
-    }
+        if(isNaN(numero)) {
+            alert("Por Favor, insira números válidos.");
+            return;
+        }
 
-    if(situacao === "verao", numero < 26) {
-        alert("Temperatura Fria ❄️");
-        return;
-    }
-    else if(situacao === "verao" )
+        if(situacao === "verao", numero <= 28) {
+            alert("Temperatura Fria 🥶");
+            return;
+        }
+        else if(situacao === "verao", numero >=29 && numero <=33 ){
+            alert("Temperatura Agradável 😊 ");
+            return;
+        }
+        else if(situacao === "verao", numero >=34){
+            alert("Temperatura Quente 🥵");
+            return;
+        }
+
+        if (situacao === "inverno", numero <= 30) {
+            alert("Temperatura Fria ")
+        }
+
 })
