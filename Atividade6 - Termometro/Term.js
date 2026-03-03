85e34148b4ab9a2bd182a53ea5ec5fe6125af9cb
Arthur Alencar <arthuralencarpinheiro844@gmail.com>
2026-03-02 22:00:44 -0300

    .

--- Atividade6 - Termometro/Term.js
+++ Atividade6 - Termometro/Term.js
@@ -1,27 +1,28 @@
-document.getElementById("verificarBtn").addEventListener("click", function() {
+document.getElementById("verificarBtn").addEventListener("click", function () {
     let numero = parseInt(document.getElementById("numero").value);
-    let situacao = parseInt(document.getElementById("situacao").value);
+    let situacao = document.getElementById("situacao").value;
 
-        if(isNaN(numero)) {
-            alert("Por Favor, insira números válidos.");
-            return;
-        }
+    if (isNaN(numero)) {
+        alert("Por favor, insira números válidos.");
+        return;
+    }
 
-        if(situacao === "verao", numero <= 28) {
-            alert("Temperatura Fria 🥶");
-            return;
-        }
-        else if(situacao === "verao", numero >=29 && numero <=33 ){
-            alert("Temperatura Agradável 😊 ");
-            return;
-        }
-        else if(situacao === "verao", numero >=34){
-            alert("Temperatura Quente 🥵");
-            return;
-        }
-
-        if (situacao === "inverno", numero <= 30) {
-            alert("Temperatura Fria ")
-        }
-
-})
+    if (situacao === "verao" && numero <= 28) {
+        alert(numero + "° graus: Temperatura Fria 🥶");
+    }
+    else if (situacao === "verao" && numero >= 29 && numero <= 33) {
+        alert(numero + "° graus: Temperatura Agradável 😊");
+    }
+    else if (situacao === "verao" && numero >= 34) {
+        alert(numero + "° graus: Temperatura Quente 🥵");
+    }
+    else if (situacao === "inverno" && numero <= 29) {
+        alert(numero + "° graus: Temperatura Fria 🥶");
+    }
+    else if (situacao === "inverno" && numero >= 30 && numero <= 37) {
+        alert(numero + "° graus: Temperatura Agradável 😊");
+    }
+    else if (situacao === "inverno" && numero >= 38) {
+        alert(numero + "° graus: Temperatura Quente 🥵");
+    }
+});
